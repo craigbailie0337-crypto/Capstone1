18ca052a2b072c3396c74d0b4593622679975d2a
Craig <Craig@MacBook-Pro.local>
2026-04-27 20:12:02 -0600

    Adding Recent cases test code

--- test/specs/RecentCases.test.js
+++ test/specs/RecentCases.test.js
@@ -4,5 +4,44 @@ import RecentCasesPage from '../pageobjects/RecentCases.page.js';
 import Page from '../pageobjects/page.js';
 
 describe('Recent Cases Feature', () => {
-    before(async () => {})
-})
+    
+    before(async () => {
+        await Page.opening();
+        await LoginPage.login(SensitiveInfo.username, SensitiveInfo.password);
+        await browser.pause(2000);
+    });
+
+    it('MTQA-5504: Open Case- verify Case Top Of Recentcases list', async () => {
+        await RecentCasesPage.openCaseAndVerifyAtTopOfRecentCases();
+        const cases = await RecentCasesPage.recentCaseItems;
+        await expect(cases[0]).toBeDisplayed();
+    });
+
+    it('MTQA-5528: View Case A Then Case B- Case B At Top Of Recentcases', async () => {
+        await RecentCasesPage.viewCaseAThenCaseB();
+        await RecentCasesPage.recentCasesNavButton.click();
+        await browser.pause(600);
+        const cases = await RecentCasesPage.recentCaseItems;
+        await expect(cases[0]).toBeDisplayed();
+
+    });
+
+    it('MTQA-5529: View 6 Cases- 6th Case Pushes 5th Off List', async () => {
+        await RecentCasesPage.viewSixCases();
+        await RecentCasesPage.recentCasesNavButton.click();
+        await browser.pause(600);
+        const cases = await RecentCasesPage.recentCaseItems;
+        await expect(cases.length).toBeLessThanOrEqual(5);
+        
+    });
+
+    it('MTQA-5541: View Then Delete The Case- Case Is Removed From Recentcases', async () => {
+        await RecentCasesPage.openCaseDeleteAndVerifyRemovedFromRecentCases();
+        const cases = await RecentCasesPage.recentCaseItems;
+        await expect(cases.length).toBeGreaterThanOrEqual(0);
+        
+    });
+
+
+
+});
